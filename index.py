5218fee882a3ed6445550f70a0a5a24433f233ce
Bipul Dawadi <bipuldawadi14@gmail.com>
2023-08-31 20:28:13 +0545

    Function

--- index.py
+++ index.py
@@ -1,33 +1,33 @@
-students={
-    'Bibek':{
-    'courses':['Computer',"math",'DSA'],
-    'grade':['A+',"A",'A+']
-    },
+# students={
+#     'Bibek':{
+#     'courses':['Computer',"math",'DSA'],
+#     'grade':['A+',"A",'A+']
+#     },
         
-    'Bipul':
-    {
-    'courses':["DSA",'ComputerNetworks','DDA'],
-    'grade':[
-        {
-        "DSA":'A+',"ComputerNetworks":'A+',"DDA":'A+'
-        }
-        ]
-},
+#     'Bipul':
+#     {
+#     'courses':["DSA",'ComputerNetworks','DDA'],
+#     'grade':[
+#         {
+#         "DSA":'A+',"ComputerNetworks":'A+',"DDA":'A+'
+#         }
+#         ]
+# },
         
-   'Bimal':{
-    'courses':["Economy",'Nepali','english'],
-    'grade':[{'Economy':'B+',
-              'Nepali':"A",
-              'english':'A'}]
-},
+#    'Bimal':{
+#     'courses':["Economy",'Nepali','english'],
+#     'grade':[{'Economy':'B+',
+#               'Nepali':"A",
+#               'english':'A'}]
+# },
     
-'Gaunter':{
-    'courses':['English','Finance','Sociology'],
-    'grade':[{"English":'B',
-    "Finance":"A",
-    'Sociology':'A'}]
-}
-}
-# print(students)
-print(students['Bipul'][0]['grade'])
+# 'Gaunter':{
+#     'courses':['English','Finance','Sociology'],
+#     'grade':[{"English":'B',
+#     "Finance":"A",
+#     'Sociology':'A'}]
+# }
+# }
+# # print(students)
+# print(students['Bipul'][0]['grade'])
 
